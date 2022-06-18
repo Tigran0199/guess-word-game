cda02e9d6c04dd550935a512d16ac87a2e8378f4
Tigran0199 <cruelman11@gmail.com>
2022-06-18 12:55:56 +0400

    feat : Added navigation option for main menu

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import styles from './MainMenu.module.css'
 import MenuLogo from '../../assets/images/kid.jpg'
+import { useNavigate } from 'react-router-dom'
 
 export default function MainMenu() {
+    const navigate = useNavigate("")
+    function start(){
+        navigate("/play")
+    }
+    function score(){
+        navigate("/score")
+    }
+    function info(){
+        navigate("/info")
+    }
+    function SignInUp(){
+        navigate("/signInUp")
+    }
   return (
     <div className={styles.menuContainer}>
         <img src={MenuLogo}  className={styles.menuLogo}/>
-        <button className={styles.menuButton}>Սկսել</button>
-        <button className={styles.menuButton}>Հաշիվ</button>
-        <button className={styles.menuButton}>Ինֆո</button>
+        <button className={styles.menuButton} onClick={start}>Սկսել</button>
+        <button className={styles.menuButton} onClick={score}>Հաշիվ</button>
+        <button className={styles.menuButton} onClick={info}>Ինֆո</button>
+        <button className={styles.menuButton} onClick={SignInUp}>Մուտք / Գրանցում</button>
     </div>
   )
 }
