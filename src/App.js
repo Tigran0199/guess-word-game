dae9d50bde170fe192aa2a98de493c56cb2f06d5
Tigran0199 <cruelman11@gmail.com>
2022-06-18 12:35:06 +0400

    feat: Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import  './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import loader from './assets/loaders/mona.gif'
+import MainMenu from "./components/MainMenu/MainMenu"
 
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<MainMenu />} />
+          </Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
